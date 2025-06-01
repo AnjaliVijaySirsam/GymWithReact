5903021fc8b6c73aec91ebe25829889c2f417efc
AnjaliVijaySirsam <anjalisirsam97@gmail.com>
2025-06-01 16:15:31 +0530

    Updates

---
+++ precache-manifest.cb77c7ba68df57533bbbeac35236c7c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "267e5b3399d4371d5480",
+    "url": "/GymWithReact/static/css/main.86984d80.chunk.css"
+  },
+  {
+    "revision": "267e5b3399d4371d5480",
+    "url": "/GymWithReact/static/js/main.267e5b33.chunk.js"
+  },
+  {
+    "revision": "4409fe34ec5debf874b0",
+    "url": "/GymWithReact/static/js/1.4409fe34.chunk.js"
+  },
+  {
+    "revision": "a3f8ed8e43e894643b64",
+    "url": "/GymWithReact/static/js/runtime~main.a3f8ed8e.js"
+  },
+  {
+    "revision": "3b60236561f786e9096c0564eb57dbd7",
+    "url": "/GymWithReact/static/media/gym.3b602365.png"
+  },
+  {
+    "revision": "ba78eef3a8e48d5ee3fcf86ce0488b5d",
+    "url": "/GymWithReact/static/media/banner.ba78eef3.png"
+  },
+  {
+    "revision": "21febce70ab1a9b8ea7fa2259a2f6cf3",
+    "url": "/GymWithReact/index.html"
+  }
+];
